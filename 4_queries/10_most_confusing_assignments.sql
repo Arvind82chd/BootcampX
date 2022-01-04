6681ed53f881723c474b916512005508d6d9ee44
Arvind Sharma <mail.arvind.s@gmail.com>
2022-01-04 20:03:34 +0000

    added further queries to 4

---
+++ 4_queries/10_most_confusing_assignments.sql
@@ -0,0 +1,11 @@
+-- SELECT id, name, day, chapter, COUNT(assistance_requests.*) AS total_requests 
+-- FROM assistance_requests
+-- JOIN assignments ON assignments.id = assignment_id
+-- GROUP BY assignment_id
+-- ORDER BY total_requests DESC;
+
+SELECT assignments.id, name, day, chapter, COUNT(assistance_requests.*) AS total_requests 
+FROM assignments
+JOIN assistance_requests ON assignments.id = assignment_id
+GROUP BY assignments.id
+ORDER BY total_requests DESC;
---
+++ 4_queries/11_total_assignments_duration.sql
@@ -0,0 +1,5 @@
+SELECT assignments.day, count(assignments.*) AS number_of_assignments,
+SUM(duration) AS duration
+FROM assignments
+GROUP BY assignments.day
+ORDER BY day;
