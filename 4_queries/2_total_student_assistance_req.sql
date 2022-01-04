7649479cf4402e7fed0ef70ac2c0098874b4857f
Arvind Sharma <mail.arvind.s@gmail.com>
2022-01-04 18:55:38 +0000

    added 4_queries

---
+++ 4_queries/2_total_student_assistance_req.sql
@@ -0,0 +1,9 @@
+
+
+SELECT COUNT(assistance_requests.*) AS total_assistances,
+students.name AS name
+FROM assistance_requests
+JOIN students
+ON students.id = student_id
+WHERE name = 'Elliot Dickinson'
+GROUP BY students.name;
